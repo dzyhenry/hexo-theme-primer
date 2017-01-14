d0df25da44a07ea9fdfbb0a4c8fc089f95389fa9
dzyhenry <dzyhenry@foxmail.com>
2017-01-14 14:24:49 +0800

    custom setting

--- source/js/search.js
+++ source/js/search.js
@@ -15,6 +15,9 @@ var searchFunc = function(path, search_id, content_id) {
             }).get();
             var $input = document.getElementById(search_id);
             var $resultContent = document.getElementById(content_id);
+            if (!$input) {
+              return;
+            }
             $input.addEventListener('input', function(){
                 var str='<ul class=\"search-result-list\">';                
                 var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
